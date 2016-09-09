4c60d55b789177fb4b5ab4c995d9a9314d4e44e9
L <snajah.contact@gmail.com>
2016-09-09 18:00:57 +0000

    Swing Application

---
+++ src/DetailsPanel.java
@@ -0,0 +1,78 @@
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class DetailsPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DetailsPanel() {
+		super();
+		Dimension size = getPreferredSize();
+		size.width = 250;
+		setPreferredSize(size);
+		
+		setBorder(BorderFactory.createTitledBorder("Personal Details"));
+		
+		JLabel nameLabel = new JLabel("Name: ");
+		JLabel occupationLabel = new JLabel("Occupation: ");
+		
+		JTextField nameField = new JTextField(10);
+		JTextField occupationField = new JTextField(10);
+		
+		JButton addBtn = new JButton("ADD");
+		
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		// Recommendation when creating stuff manually
+		// Classify the controls into logical sections in your mind
+		// and put big comments in each section in the code.
+		
+		
+		// First column
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.weighty = 0.5;
+		gc.weightx = 0.5;
+		
+		gc.gridx=0;
+		gc.gridy=0;
+		add(nameLabel, gc);
+		
+		gc.gridx = 0;
+		gc.gridy = 1;
+		add(occupationLabel, gc);
+		
+		// Second column
+		gc.anchor = GridBagConstraints.LINE_START;
+		gc.gridx = 1;
+		gc.gridy = 0;
+		add(nameField, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 1;
+		add(occupationField, gc);
+		
+		// Final row
+		gc.weighty = 10;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.gridx = 1;
+		gc.gridy = 2;
+		add(addBtn, gc);
+		
+		
+	}
+	
+	
+
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -13,6 +13,7 @@ public class MainFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private DetailsPanel detailsPanel;
 
 	public MainFrame(String title) {
 		super(title);
@@ -24,10 +25,14 @@ public class MainFrame extends JFrame {
 		JTextArea textArea = new JTextArea();
 		JButton button = new JButton("Click me");
 		
+		detailsPanel = new DetailsPanel();
+		
 		// Step 3 - Add Swing components to content panel.
 		Container c = getContentPane();
 		c.add(textArea, BorderLayout.CENTER);
 		c.add(button, BorderLayout.SOUTH);
+		c.add(detailsPanel, BorderLayout.WEST);
+		
 		
 		// Add behaviour to the components.
 		button.addActionListener(new ActionListener() {
