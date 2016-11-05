e791faa722cb74a210c799ed44df3552d35ac886
L <snajah.contact@gmail.com>
2016-11-05 14:40:09 +0000

    Modification

--- src/App.java
+++ src/App.java
@@ -5,16 +5,20 @@ public class App {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-			
+
 			@Override
 			public void run() {
-			
+
 				JFrame frame = new MainFrame("Hello World SWING");
 
 				frame.setSize(500, 400);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
-		   }
-		});		
+			}
+		});
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Roger FEDERER");
+		}
 	}
 }
