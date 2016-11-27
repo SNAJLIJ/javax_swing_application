6d20705645db173ad64a354eb3f9eee579c51650
L <snajah.contact@gmail.com>
2016-11-27 22:04:51 +0000

    Thread

--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -3,7 +3,8 @@ public class MyRunnable implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		for(int i=0;i<6;i++) {
+			System.out.println("Runnable running");
+		}
 	}
 }
